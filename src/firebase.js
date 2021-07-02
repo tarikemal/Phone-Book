b2c98910a67e59ccce04a3e382e98ca778fb4bfb
T.Kemal <tarikkemal314@gmail.com>
2021-07-02 13:05:03 +0300

    firebase config information hidden

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGCBuZn8KTtTiejmGcZgUWEAXSoyXW-dM",
-  authDomain: "react-phone-book-1f29a.firebaseapp.com",
-  projectId: "react-phone-book-1f29a",
-  storageBucket: "react-phone-book-1f29a.appspot.com",
-  messagingSenderId: "124443298154",
-  appId: "1:124443298154:web:ef69221308acd6eea27d13",
-  measurementId: "G-6P9BVGJFQZ",
+  // config
 };
 
 if (!firebase.apps.length) {
